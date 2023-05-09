486cc0dc2cfd672410d6138f5d238ddfc4c150ae
justin-aquino <justin.aquino25@gmail.com>
2023-05-08 17:51:26 -0700

    adding placeholder functions

--- app/(site)/Components/AuthForm.tsx
+++ app/(site)/Components/AuthForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -16,8 +17,36 @@ const AuthForm = () => {
   }
  }, [variant]);
 
+ const {
+  register,
+  handleSubmit,
+  formState: { errors },
+ } = useForm<FieldValues>({
+  defaultValues: {
+   name: "",
+   email: "",
+   password: "",
+  },
+ });
+
+ const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  setIsLoading(true);
+
+  if (variant === "REGISTER") {
+   console.log("register variant");
+  }
+
+  if (variant === "LOGIN") {
+   console.log("Next Auth");
+  }
+ };
+
+ const socialAction = (action: string) => {
+  setIsLoading(true);
+ };
+
  return (
-  <div>
+  <div className="">
    <div></div>
   </div>
  );
