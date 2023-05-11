3965384e8b4f66d0f7a23943e973bd8abef5dfc1
justin-aquino <justin.aquino25@gmail.com>
2023-05-10 21:59:52 -0700

    adding submit button

--- app/(site)/Components/AuthForm.tsx
+++ app/(site)/Components/AuthForm.tsx
@@ -3,59 +3,85 @@
 import Input from "@/app/Components/Inputs/Input";
 import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../../Components/Button";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
- const [variant, setVariant] = useState<Variant>("LOGIN");
- const [isLoading, setIsLoading] = useState(false);
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
 
- const toggleVariant = useCallback(() => {
-  if (variant === "LOGIN") {
-   setVariant("REGISTER");
-  } else {
-   setVariant("LOGIN");
-  }
- }, [variant]);
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
 
- const {
-  register,
-  handleSubmit,
-  formState: { errors },
- } = useForm<FieldValues>({
-  defaultValues: {
-   name: "",
-   email: "",
-   password: "",
-  },
- });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
- const onSubmit: SubmitHandler<FieldValues> = (data) => {
-  setIsLoading(true);
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
 
-  if (variant === "REGISTER") {
-   console.log("register variant");
-  }
+    if (variant === "REGISTER") {
+      console.log("register variant");
+    }
 
-  if (variant === "LOGIN") {
-   console.log("Next Auth");
-  }
- };
+    if (variant === "LOGIN") {
+      console.log("Next Auth");
+    }
+  };
 
- const socialAction = (action: string) => {
-  setIsLoading(true);
- };
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
 
- return (
-  <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-   <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-     {/* wrap onSubmit in handleSubmit to get data from useForm hook */}
-     <Input label="Email" register={register} id="email" errors={errors} />
-    </form>
-   </div>
-  </div>
- );
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              label="Email"
+              type="email"
+              register={register}
+              id="email"
+              errors={errors}
+            />
+          )}
+
+          <Input
+            label="Email Address"
+            register={register}
+            id="email"
+            type="email"
+            errors={errors}
+          />
+          <Input
+            label="Password"
+            register={register}
+            id="password"
+            errors={errors}
+            type="password"
+          />
+          <Button></Button>
+
+          {/* wrap onSubmit in handleSubmit to get data from useForm hook */}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
---
+++ app/Components/Button.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import clsx from "clsx";
+
+const Button = () => {
+  return <div>Button</div>;
+};
+
+export default Button;
