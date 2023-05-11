9e0fb2159d066d1b850fc1a9091b9626722f42a0
justin-aquino <justin.aquino25@gmail.com>
2023-05-10 22:18:13 -0700

    initializing auth social button component

--- app/(site)/Components/AuthForm.tsx
+++ app/(site)/Components/AuthForm.tsx
@@ -4,6 +4,7 @@ import Input from "@/app/Components/Inputs/Input";
 import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../../Components/Button";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -75,10 +76,28 @@ const AuthForm = () => {
             errors={errors}
             type="password"
           />
-          <Button>Test</Button>
+          <Button disabled={isLoading} fullWidth type="submit">
+            {variant === "LOGIN" ? "Sign In" : "Register"}
+          </Button>
 
           {/* wrap onSubmit in handleSubmit to get data from useForm hook */}
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset09 flex items-center">
+              <hr className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton />
+          </div>
+        </div>
       </div>
     </div>
   );
