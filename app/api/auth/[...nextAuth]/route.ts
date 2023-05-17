86d072af4722c3f7cc9f9aee476bbffec7c21bd9
justin-aquino <justin.aquino25@gmail.com>
2023-05-17 13:52:10 -0700

    adding NextAuth

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -17,17 +17,48 @@ export const authOptions: AuthOptions = {
    clientId: process.env.GOOGLE_CLIENT_ID as string,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
   }),
+
   CredentialsProvider({
    name: "credentials",
    credentials: {
     email: { label: "email", type: "text" },
     password: { label: "password", type: "password" },
-    async authorize(credentials) {
-     if (!credentials?.email || !credentials?.password) {
-      throw new Error("Invalid Credentials");
-     }
-    },
+   },
+
+   async authorize(credentials) {
+    if (!credentials?.email || !credentials?.password) {
+     throw new Error("Invalid Credentials");
+    }
+
+    const user = await prisma?.user.findUnique({
+     where: {
+      email: credentials.email,
+     },
+    });
+
+    if (!user || !user?.hashedPassword) {
+     throw new Error("Invalid Credentials");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(
+     credentials.password,
+     user.hashedPassword
+    );
+
+    if (!isCorrectPassword) {
+     throw new Error("Invalid credentials");
+    }
+    return user;
    },
   }),
  ],
+ debug: process.env.NODE_ENV === "development",
+ session: {
+  strategy: "jwt",
+ },
+ secret: process.env.NEXT_AUTH_SECRET,
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
